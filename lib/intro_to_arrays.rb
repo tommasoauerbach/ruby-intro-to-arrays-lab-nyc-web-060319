5357f452f288e9349c2418810b57ec4e8ecc7404
tommaso auerbach <tsa302@nyu.edu>
2019-06-02 18:59:15 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -12,3 +12,8 @@ def first_element(my_first_element)
   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   my_first_element = @taylor_swift.first
 end
+
+def third_element(my_third_element)
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_third_element = @taylor_swift[3]
+end
