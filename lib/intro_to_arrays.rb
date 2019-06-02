ba8a9617f77ae602657a405040cf72870bbddc51
tommaso auerbach <tsa302@nyu.edu>
2019-06-02 19:03:16 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -22,3 +22,8 @@ def last_element(my_last_element)
   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   my_last_element = @taylor_swift[-1]
 end
+
+def first_element_with_array_methods(first_rapper)
+  @nyc_rappers = ["Nas", "Biggie", "Flatbush", "Wutang"]
+  first_element_with_array_methods = @nyc_rappers.first
+end
