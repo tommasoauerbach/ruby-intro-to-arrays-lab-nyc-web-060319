91b426f127d4bc8c286a29beea057093115bbb8a
tommaso auerbach <tsa302@nyu.edu>
2019-06-02 19:01:07 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -17,3 +17,8 @@ def third_element(my_third_element)
   @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
   my_third_element = @taylor_swift[2]
 end
+
+def last_element(my_last_element)
+  @taylor_swift = ["Welcome to New York", "Blank Space", "Style", "Out of The Woods"]
+  my_last_element = @taylor_swift[-1]
+end
